f65924e59a08539f19d36b059ef181b5d814ac1c
root <root@silence.me>
2017-12-05 18:01:09 +0800

    update: 2017-12-05 18:01:08

---
+++ log.go
@@ -0,0 +1,218 @@
+package log
+
+import (
+	"errors"
+	"fmt"
+	"os"
+	"runtime"
+	"strconv"
+	"sync"
+	"time"
+)
+
+/*
+ 开发日志：
+ 1. 替换 fmt.Sprintf => []byte(三个地方) 节省大约800ns
+*/
+
+const (
+	/*terminal color format
+	"\x1b[0;%dm%s\x1b[0m"
+	*/
+	TerminalColorBlack = iota + 30
+	TerminalColorRed
+	TerminalColorGreen
+	TerminalColorYellow
+	TerminalColorBlue
+	TerminalColorMagenta
+	TerminalColorCyan
+	TerminalColorWhite
+)
+
+const (
+	/*log level*/
+	LevelDebug = 0
+	LevelInfo  = 1
+	LevelWarn  = 2
+	LevelError = 3
+)
+
+func init() {
+	/**/
+}
+
+type Logger struct {
+	mux          sync.Mutex
+	outputs      []*os.File
+	level        int
+	calldepth    int
+	timeformat   string
+	timeinterval int64 //unit: seconds
+	maxsize      int64
+
+	inittime time.Time
+}
+
+func NewLogger(f []*os.File, level int, timeformat string) *Logger {
+	if len(f) == 0 {
+		f = []*os.File{os.Stdout}
+	}
+
+	if level < LevelDebug || level > LevelError {
+		level = LevelInfo
+	}
+
+	if timeformat == "" {
+		timeformat = "2006-01-02 15:04:05.000"
+	}
+
+	return &Logger{
+		outputs:      f,
+		level:        level,
+		calldepth:    2,
+		timeformat:   timeformat,
+		timeinterval: 1, //当值大于0秒时，按间隔计算，否则按照文件大小计算
+		inittime:     time.Now(),
+	}
+}
+
+func (l *Logger) Output(prefix, logstr string, color int) error {
+	var now = time.Now()
+
+	l.mux.Lock()
+	defer l.mux.Unlock()
+
+	var buf []byte
+
+	/*logstr format*/
+	var tfmt = now.Format(l.timeformat)
+
+	var _, file_name, line_number, ok = runtime.Caller(l.calldepth)
+	if !ok {
+		return errors.New("runtime caller false.")
+	} else {
+		for i := len(file_name) - 1; i > 0; i-- {
+			if file_name[i] == '/' {
+				file_name = file_name[i+1:]
+				break
+			}
+		}
+	}
+
+	buf = append(buf, prefix...)
+	buf = append(buf, tfmt...)
+	buf = append(buf, " "...)
+	buf = append(buf, file_name...)
+	buf = append(buf, ":"...)
+	buf = append(buf, strconv.Itoa(line_number)...)
+	buf = append(buf, " ▸ "...)
+	buf = append(buf, logstr...)
+
+	//var _, err = l.Write(&buf, now, color)
+	//write ...
+
+	for i, f := range l.outputs {
+		var fd = f.Fd()
+		var name = f.Name() //full name
+		var stat, stat_err = f.Stat()
+		if stat_err != nil {
+			return stat_err
+		}
+		var size = stat.Size()
+
+	}
+	return nil
+}
+
+/*
+ @param(color): terminal color
+ @param(s)    : output string
+*/
+func (l *Logger) Write(b *[]byte, time time.Time, color int) (int, error) {
+	for i, f := range l.outputs {
+		var fd = f.Fd()
+		var name = f.Name()
+
+		var final_buf []byte
+		if (fd == 1 && name == os.Stdout.Name()) || (fd == 2 && name == os.Stderr.Name()) {
+			if TerminalColorBlack <= color && color <= TerminalColorWhite {
+				final_buf = append(final_buf, "\x1b[0;"...)
+				final_buf = append(final_buf, strconv.Itoa(color)...)
+				final_buf = append(final_buf, "m"...)
+				final_buf = append(final_buf, *b...)
+				final_buf = append(final_buf, "\x1b[0m"...)
+			} else {
+				final_buf = append(final_buf, *b...)
+			}
+		} else {
+			if l.timeinterval > 0 && (time.Unix()-l.inittime.Unix() != 0) && (time.Unix()-l.inittime.Unix())%l.timeinterval == 0 {
+				var _ = f.Close()
+				var _ = os.Rename(name, fmt.Sprintf("%s.bak.%d", name, time.Unix))
+				var nf, _ = os.OpenFile(name, os.O_APPEND|os.O_CREATE|os.O_RDWR, 0666)
+				l.outputs[i] = nf
+				f = nf
+			}
+
+			fmt.Println(f.Stat().Size())
+
+			final_buf = append(final_buf, *b...)
+		}
+
+		var _, err = f.Write(final_buf)
+		if err != nil {
+			return i + 1, err
+		}
+	}
+
+	return len(l.outputs), nil
+}
+
+func create_bak_file(old, new string) {
+
+}
+
+func (l *Logger) Debug(v ...interface{}) {
+	if l.level <= LevelDebug {
+		var prefix = "[DEBUG] "
+		var s = fmt.Sprintln(v...)
+		var err = l.Output(prefix, s, TerminalColorGreen)
+		if err != nil {
+			fmt.Println(err.Error())
+		}
+	}
+}
+
+func (l *Logger) Info(v ...interface{}) {
+	if l.level <= LevelInfo {
+		var prefix = "[Info] "
+		var s = fmt.Sprintln(v...)
+		var err = l.Output(prefix, s, TerminalColorWhite)
+		if err != nil {
+			fmt.Println(err.Error())
+		}
+	}
+}
+
+func (l *Logger) Warn(v ...interface{}) {
+	if l.level <= LevelWarn {
+		var prefix = "[Warn] "
+		var s = fmt.Sprintln(v...)
+		var err = l.Output(prefix, s, TerminalColorYellow)
+		if err != nil {
+			fmt.Println(err.Error())
+		}
+	}
+}
+
+func (l *Logger) Error(v ...interface{}) {
+	if l.level <= LevelError {
+		var prefix = "[Error] "
+		var s = fmt.Sprintln(v...)
+		var err = l.Output(prefix, s, TerminalColorRed)
+		if err != nil {
+			fmt.Println(err.Error())
+		}
+
+		panic(s)
+	}
+}
---
+++ log_test.go
@@ -0,0 +1,62 @@
+package log
+
+import (
+	"log"
+	"os"
+	"testing"
+)
+
+/*
+export GOPATH=/root/devel/golang/go-libs/
+go test go-log -v -test.run Test_Output
+*/
+func Test_Output(t *testing.T) {
+	var fs = []*os.File{
+		os.Stdout,
+	}
+
+	var logger = NewLogger(fs, LevelDebug, "2006-01-02 15:04:05.000")
+
+	logger.Debug("12", "asd")
+	logger.Info("12", "asd")
+	logger.Warn("12", "asd")
+	//logger.Error("12", "asd")
+}
+
+/*
+ go test go-log -v -test.bench Benchmark_Mylog
+
+
+  200000	     11370 ns/op
+PASS
+ok  	go-log	2.383s
+*/
+func Benchmark_Mylog(b *testing.B) {
+	var f, _ = os.OpenFile("/tmp/testme.log", os.O_APPEND|os.O_CREATE|os.O_RDWR, 0666)
+
+	var fs = []*os.File{
+		f,
+	}
+
+	var logger = NewLogger(fs, LevelDebug, "2006-01-02 15:04:05.000")
+
+	for i := 0; i < b.N; i++ {
+		logger.Debug("benchmark test测试")
+	}
+}
+
+/*
+
+  300000	      5071 ns/op
+PASS
+ok  	go-log	1.575s
+
+*/
+func Benchmark_Syslog(b *testing.B) {
+	var f, _ = os.OpenFile("/tmp/testsys.log", os.O_APPEND|os.O_CREATE|os.O_RDWR, 0666)
+	var l = log.New(f, "", log.LstdFlags|log.Lshortfile)
+
+	for i := 0; i < b.N; i++ {
+		l.Println("benchmark test测试")
+	}
+}
